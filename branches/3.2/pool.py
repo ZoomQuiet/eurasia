076d6ee042291506caa88b8d489480579112fcce
wilhelm.shen <wilhelm.shen@f9de07fb-6634-0410-b212-ef1cdd1c6ea5>
2012-08-10 09:01:44 +0000

    使用 pool.py 重新实现 fs.py

--- branches/3.2/pool.py
+++ branches/3.2/pool.py
@@ -1,68 +1,73 @@
 def apply(func, args=(), kwargs={}, timeout=-1):
-    co  = getcurrent()
-    id_ = c_uint(id(co)).value
+    back_ = getcurrent()
+    id_   = c_uint(id(back_)).value
+    data  = stack()
+    data.back_  = back_
+    data.func   = func
+    data.args   = args
+    data.kwargs = kwargs
     if -1 == timeout:
-        data = [co, None, func, args, kwargs]
-        objects[id_] = data
-        req = eio_custom (c_execute, 0, c_cb, id_)
-        contents = req.contents
+        objects[id_] = ref(data)
         try:
-            co.parent.switch()
+            req = eio_custom(c_execute, 0, c_cb, id_)
+            contents = req.contents
+            back_.parent.switch()
             if contents.cancelled:
                 raise default_cancelled
             if 0 == contents.result:
-                return data[1]
-            raise data[1]
+                return data.result
+            raise data.exception
         finally:
             del objects[id_]
     else:
-        data = [co, None, func, args, kwargs, 0]
-        objects[id_] = data
         timer1 = ev_timer()
         memmove(byref(timer1), timer0, sizeof_timer)
-        timer1.at   = timeout
-        timer1.data = id_
+        timer1.at = timeout
+        timer1.data  = id_
+        objects[id_] = ref(data)
         ev_timer_start(EV_DEFAULT_UC, byref(timer1))
-        req = eio_custom (c_execute_t, 0, c_cb, id_)
-        contents = req.contents
         try:
+            req = eio_custom(c_execute, 0, c_cb, id_)
+            contents = req.contents
             try:
-                co.parent.switch()
+                back_.parent.switch()
             except Timeout:
-                if contents.cancelled:
-                    raise default_cancelled
-                elif 0 == data[5]:
+                if 1 == data.readystate:
+                    wait = Wait()
+                    wait.get_stack = ref(data)
+                    raise wait
+                if 0 == data.readystate:
+                    if contents.cancelled:
+                        raise default_cancelled
                     eio_cancel(req)
-                    raise
-                elif 1 == data[5]:
-                    e = Running()
-                    e.data = data
-                    raise e
-                elif 2 == data[5]:
-                    pass
-                else:
                     raise
             if contents.cancelled:
                 raise default_cancelled
             if 0 == contents.result:
-                return data[1]
-            raise data[1]
+                return data.result
+            raise data.exception
         finally:
-            ev_timer_stop(EV_DEFAULT_UC, byref(timer1))
+            ev_timer_stop (EV_DEFAULT_UC, byref(timer1))
             del objects[id_]
 
-import os, sys
+class stack:
+    back_  = func = args = kwargs = None
+    result = exception = None
+    readystate = 0
+
+import sys
 from pyev  import *
 from pyeio import *
+from weakref import ref
+from errno import ETIMEDOUT
 from greenlet import getcurrent
 from traceback import print_exc
-from collections import namedtuple
-from exceptions_ import Cancelled, Running, Unknow
+from exceptions_ import Cancelled, Wait, Unknow
 
 def cb(req):
     id_ = req.contents.data
     try:
-        objects[id_][0].switch()
+        objects[id_]().back_.switch()
     except:
         print_exc(file=sys.stderr)
     return 0
@@ -72,51 +77,40 @@ c_cb = eio_cb(cb)
 def execute(req):
     contents = req.contents
     id_      = contents.data
-    data     = objects[id_]
     try:
-        res = data[2](*data[3], **data[4])
-    except BaseException, err:
-        contents.result = -1
-        data[1] = err
+        data = objects[id_]()
+        data.readystate = 1
+        try:
+            res = data.func(*data.args, **data.kwargs)
+        except BaseException, err:
+            data.exception = err
+            contents.result = -1
+            data.readystate =  2
+        except:
+            data.exception = default_unknow
+            contents.result = -1
+            data.readystate =  2
+        else:
+            data.result = res
+            data.readystate =  2
     except:
-        contents.result = -1
-        data[1] = default_unknow
-    else:
-        contents.result =  0
-        data[1] = res
+        print_exc(file=sys.stderr)
     return 0
 
 c_execute = eio_custom.argtypes[0](execute)
 
-def execute_t(req):
-    contents = req.contents
-    id_      = req.contents.data
-    data     = objects[id_]
-    data[5] = 1
-    try:
-        res = data[2](*data[3], **data[4])
-    except BaseException, err:
-        contents.result = -1
-        data[1] = err
-        data[5] = 2
-    except:
-        contents.result = -1
-        data[1] = default_unknow
-        data[5] = 2
-    else:
-        contents.result =  0
-        data[1] = res
-        data[5] = 2
-    return 0
-
 def timer_cb(l, w, e):
-    id_  = w.contents.data
+    id_ = w.contents.data
     try:
-        objects[id_].g.throw(
+        objects[id_]().back_.throw(
             Timeout,
-            Timeout(ETIMEOUT, 'operation timed out'))
+            Timeout(ETIMEDOUT, 'operation timed out'))
     except:
         print_exc(file=sys.stderr)
+
+for k, v in ev_timer._fields_:
+    if 'cb' == k:
+        c_timer_cb = v(timer_cb)
 
 def get_timer0():
     timer1 = ev_timer()
@@ -126,13 +120,9 @@ def get_timer0():
     memmove(buf, byref(timer1), sizeof_timer)
     return buf
 
-for k, v in ev_timer._fields_:
-    if 'cb' == k:
-        c_timer_cb = v(timer_cb)
-
-c_execute_t = eio_custom.argtypes[0](execute_t)
-default_cancelled = Cancelled()
-default_unknow    = Unknow()
 sizeof_timer = sizeof(ev_timer)
 timer0 = get_timer0(); del get_timer0, k, v
+
+default_cancelled = Cancelled()
+default_unknow    = Unknow()
 objects = {}
