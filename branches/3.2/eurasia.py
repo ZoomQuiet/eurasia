30c8291cf5f4edc7f31e634512eeb4543ba3518f
wileishn@gmail.com <wileishn@gmail.com@f9de07fb-6634-0410-b212-ef1cdd1c6ea5>
2012-12-27 08:22:42 +0000

    设置 Revision

--- branches/3.2/eurasia.py
+++ branches/3.2/eurasia.py
@@ -32,7 +32,7 @@
 #
 ############################################################################
 
-revision = '$Revision: 408 $'
+revision = '$Revision$'
 revision = revision[revision.find (':')+1:revision.rfind('$')].strip()
 __version__ = '3.2:%s' % revision
 
